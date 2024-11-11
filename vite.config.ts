dabdd734715b31dc69b50ec58f1a0166cbe0210a
f0rsakeN-afk <nar3sh0@gmail.com>
2024-11-11 21:12:05 +0545

    added axios

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
