117050bfcb35d465a2f8e5652e481e21fd492470
f0rsakeN-afk <nar3sh0@gmail.com>
2024-11-12 08:36:39 +0545

    updated types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,11 @@
 export interface CurrencyExchangeData {
   result: string;
   documentation: string;
-  terma_of_use: string;
+  terms_of_use: string;
   time_last_update_unix: number;
   last_time_update_utc: string;
-  time_next_update_unix: string;
+  time_next_update_unix: number;
+  time_next_update_utc: string;
   base_code: string;
   conversion_rates: {
     [currencyCode: string]: number;
