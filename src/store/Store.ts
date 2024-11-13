25041d7313cdc61b1328806be7e0b83f25bcfff8
f0rsakeN-afk <nar3sh0@gmail.com>
2024-11-13 22:02:42 +0545

    configuration done for codeSlice

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import StandardData from "./StandardConversionSlice";
+import CodeTypes from "./CodesSlice";
 
 export const store = configureStore({
   reducer: {
     stData: StandardData,
+    cdData: CodeTypes,
   },
 });
 
