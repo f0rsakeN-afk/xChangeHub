c03e3c62684f9c2fcf2a9eeab55dc92eb1e0c651
f0rsakeN-afk <nar3sh0@gmail.com>
2024-11-13 23:21:36 +0545

    change param

--- src/services/fetchCurrencyExchangeData.ts
+++ src/services/fetchCurrencyExchangeData.ts
@@ -3,11 +3,11 @@ import axios from "axios";
 
 export const fetchCurrencyExchangeData = createAsyncThunk(
   "fetch/currencyExchangeData",
-  async () => {
+  async (baseCurrency?: string) => {
     const response = await axios.get(
       `https://v6.exchangerate-api.com/v6/${
         import.meta.env.VITE_API_KEY
-      }/latest/USD`
+      }/latest/${baseCurrency || "USD"}`
     );
     return response.data;
   }
