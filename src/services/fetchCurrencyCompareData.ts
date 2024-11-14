21c36a6585466512b84769e69d83e7d25d66f2fd
f0rsakeN-afk <nar3sh0@gmail.com>
2024-11-14 21:23:42 +0545

    changed url structure

--- src/services/fetchCurrencyCompareData.ts
+++ src/services/fetchCurrencyCompareData.ts
@@ -9,12 +9,16 @@ interface fetchCurrencyCompareDataProps {
 
 export const fetchCurrencyCompareData = createAsyncThunk(
   "fetch/currencyExchangeData",
-  async ({ base_code, target_code }: fetchCurrencyCompareDataProps) => {
-    const response = await axios.get(
-      `https://v6.exchangerate-api.com/v6/${
-        import.meta.env.VITE_API_KEY
-      }/pair/${base_code}/${target_code}`
-    );
+  async ({ base_code, target_code, amount }: fetchCurrencyCompareDataProps) => {
+    let url = `https://v6.exchangerate-api.com/v6/${
+      import.meta.env.VITE_API_KEY
+    }/pair/${base_code}/${target_code}`;
+
+    if (amount) {
+      url = `${url}/${amount}`;
+    }
+
+    const response = await axios.get(url);
     return response.data;
   }
 );
