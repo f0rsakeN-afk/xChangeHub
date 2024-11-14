6205d14a4b4c108f4edbfd9eb4e6ba58aca65bc7
f0rsakeN-afk <nar3sh0@gmail.com>
2024-11-14 08:10:30 +0545

    changed loading

--- src/store/CodesSlice.ts
+++ src/store/CodesSlice.ts
@@ -3,13 +3,13 @@ import { fetchCurrencyCodes } from "@/services/fetchCodeTypes";
 import { CodeTypes } from "@/types";
 
 interface initialStateProps {
-  isLoading: boolean;
+  isTypesLoading: boolean;
   codeTypesData: CodeTypes | null;
   error: string | null;
 }
 
 const initialState: initialStateProps = {
-  isLoading: false,
+  isTypesLoading: false,
   codeTypesData: null,
   error: null,
 };
@@ -21,19 +21,19 @@ const CodesSlice = createSlice({
   extraReducers(builder) {
     builder
       .addCase(fetchCurrencyCodes.pending, (state) => {
-        state.isLoading = true;
+        state.isTypesLoading = true;
         state.error = null;
       })
       .addCase(
         fetchCurrencyCodes.fulfilled,
         (state, action: PayloadAction<CodeTypes>) => {
-          state.isLoading = false;
+          state.isTypesLoading = false;
           state.codeTypesData = action.payload;
           state.error = null;
         }
       )
       .addCase(fetchCurrencyCodes.rejected, (state, action) => {
-        state.isLoading = false;
+        state.isTypesLoading = false;
         state.error =
           action.error.message || "Failed  to fetch exchange codes!!";
       });
